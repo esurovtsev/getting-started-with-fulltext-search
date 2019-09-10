f10fab1bd8af5a20abf0d05ac1846cfa32c2710c
esurovtsev <eugene.sourovtsev@gmail.com>
2019-09-10 23:43:47 +0200

    introduced DocumentService.findById method

--- src/main/kotlin/com/grabduck/searchengine/DocumentService.kt
+++ src/main/kotlin/com/grabduck/searchengine/DocumentService.kt
@@ -6,6 +6,8 @@ import java.io.File
 
 interface DocumentService {
     fun findAllIds(): List<String>
+
+    fun findById(documentId: String): String?
 }
 
 @Service
@@ -15,4 +17,12 @@ class DocumentServiceImpl : DocumentService {
 
     override fun findAllIds(): List<String> =
             File(dataDir).listFiles().map { it.name }
+
+    override fun findById(documentId: String): String? =
+            this
+                .findAllIds()
+                .findLast { it == documentId }
+                ?.let { File("$dataDir/$it") }
+                ?.takeIf { it.exists() && it.isFile && it.canRead() }
+                ?.let { it.readText() }
 }
