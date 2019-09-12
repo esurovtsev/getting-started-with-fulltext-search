52be11cfb698f44539361bcb567615289045b9b9
esurovtsev <eugene.sourovtsev@gmail.com>
2019-09-12 12:14:36 +0200

    added 2 commands for performing brute force searches

--- src/main/kotlin/com/grabduck/searchengine/Commands.kt
+++ src/main/kotlin/com/grabduck/searchengine/Commands.kt
@@ -4,7 +4,11 @@ import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
 
 @ShellComponent
-class Commands(private val config: Configuration, private val documentService: DocumentService) {
+class Commands(
+        private val config: Configuration,
+        private val documentService: DocumentService,
+        private val searchService: SearchService
+) {
     @ShellMethod("Show current configuration")
     fun config(): List<String> =
             config.getAllProperties().entries.map { "${it.key}: ${it.value}" }
@@ -16,4 +20,12 @@ class Commands(private val config: Configuration, private val documentService: D
     @ShellMethod("Show document content by document ID")
     fun findById(documentId: String): String =
             documentService.findById(documentId) ?: "document not found"
+
+    @ShellMethod("Performs a brute force search across all documents using simple comparison")
+    fun findUsingBruteForceSimple(request: String): List<String> =
+            searchService.findUsingBruteForce_simple(request)
+
+    @ShellMethod("Performs a brute force search across all documents using tokenization for search request")
+    fun findUsingBruteForceTokenize(request: String): List<String> =
+            searchService.findUsingBruteForce_tokenize(request)
 }
