668bf25b6b2df4da2b657b732c0fb0a0130bad60
esurovtsev <eugene.sourovtsev@gmail.com>
2019-09-13 11:15:21 +0200

    StopwordsFilter implemented

--- src/main/kotlin/com/grabduck/searchengine/Analyzer.kt
+++ src/main/kotlin/com/grabduck/searchengine/Analyzer.kt
@@ -1,38 +1,66 @@
 package com.grabduck.searchengine
 
 import org.springframework.stereotype.Component
+import java.io.File
+import javax.annotation.PostConstruct
 
 @Component
-class TokenAnalyzer {
+class TokenAnalyzer(
+        private val whitespaceTokenizer: WhitespaceTokenizer,
+        private val betterTokenizer: BetterTokenizer,
+        private val lowerCaseFilter: LowerCaseFilter,
+        private val stopwordsFilter: StopwordsFilter
+) {
     fun tokenize(input: String): List<String> =
-            WhitespaceTokenizer.tokenize(input)
+            whitespaceTokenizer.tokenize(input)
 
     fun analyze(input: String): List<String> =
-            BetterTokenizer
+            betterTokenizer
                 .tokenize(input)
-                .flatMap { LowerCaseFilter.filter(it) }
+                .flatMap { lowerCaseFilter.filter(it) }
+                .flatMap { stopwordsFilter.filter(it) }
                 .toSortedSet()
                 .toList()
 
 }
 
-object WhitespaceTokenizer {
+@Component
+class WhitespaceTokenizer {
     private val whitespace = Regex("\\s+")
 
     fun tokenize(input: String): List<String> =
             input.split(whitespace).toSet().toList()
 }
 
-object BetterTokenizer {
+@Component
+class BetterTokenizer(private val whitespaceTokenizer: WhitespaceTokenizer) {
     private val nonAlpha = Regex("[^a-zA-Z]")
 
     fun tokenize(input: String): List<String> =
-            WhitespaceTokenizer
+            whitespaceTokenizer
                 .tokenize(nonAlpha.replace(input, " "))
                 .filter { it.length > 1 }
 }
 
-object LowerCaseFilter {
-    fun filter(input: String): List<String> =
+interface Filter {
+    fun filter(input: String): List<String>
+}
+
+@Component
+class LowerCaseFilter : Filter {
+    override fun filter(input: String): List<String> =
             listOf(input.toLowerCase())
+}
+
+@Component
+class StopwordsFilter(val config: Configuration) : Filter {
+    private lateinit var stopWords: Set<String>
+
+    @PostConstruct
+    fun init() {
+        stopWords = File(config.getStopWordsFile()).readLines().toSet()
+    }
+
+    override fun filter(input: String): List<String> =
+            if (stopWords.contains(input)) listOf() else listOf(input)
 }
--- src/test/kotlin/com/grabduck/searchengine/BetterTokenizerTest.kt
+++ src/test/kotlin/com/grabduck/searchengine/BetterTokenizerTest.kt
@@ -1,97 +1,105 @@
 package com.grabduck.searchengine
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
 import org.junit.Test
 
 
 class BetterTokenizerTest {
+    private lateinit var underTest: BetterTokenizer
+
+    @Before
+    fun init() {
+        underTest = BetterTokenizer(WhitespaceTokenizer())
+    }
+
     @Test
     fun tokenize01() {
-        assertThat(BetterTokenizer.tokenize("‘Take"))
+        assertThat(underTest.tokenize("‘Take"))
             .containsExactlyInAnyOrder("Take")
     }
 
     @Test
     fun tokenize02() {
-        assertThat(BetterTokenizer.tokenize("tight!’"))
+        assertThat(underTest.tokenize("tight!’"))
             .containsExactlyInAnyOrder("tight")
     }
 
     @Test
     fun tokenize03() {
-        assertThat(BetterTokenizer.tokenize("‘Don’t"))
+        assertThat(underTest.tokenize("‘Don’t"))
             .containsExactlyInAnyOrder("Don")
     }
 
     @Test
     fun tokenize04() {
-        assertThat(BetterTokenizer.tokenize("right!’"))
+        assertThat(underTest.tokenize("right!’"))
             .containsExactlyInAnyOrder("right")
     }
 
     @Test
     fun tokenize05() {
-        assertThat(BetterTokenizer.tokenize("Darning-needle."))
+        assertThat(underTest.tokenize("Darning-needle."))
             .containsExactlyInAnyOrder("Darning", "needle")
     }
 
     @Test
     fun tokenize06() {
-        assertThat(BetterTokenizer.tokenize("Fingers;"))
+        assertThat(underTest.tokenize("Fingers;"))
             .containsExactlyInAnyOrder("Fingers")
     }
 
     @Test
     fun tokenize07() {
-        assertThat(BetterTokenizer.tokenize("breast-pin!’"))
+        assertThat(underTest.tokenize("breast-pin!’"))
             .containsExactlyInAnyOrder("breast", "pin")
     }
 
     @Test
     fun tokenize08() {
-        assertThat(BetterTokenizer.tokenize("sealing-wax.’"))
+        assertThat(underTest.tokenize("sealing-wax.’"))
             .containsExactlyInAnyOrder("sealing", "wax")
     }
 
     @Test
     fun tokenize09() {
-        assertThat(BetterTokenizer.tokenize("sha’n’t"))
+        assertThat(underTest.tokenize("sha’n’t"))
             .containsExactlyInAnyOrder("sha")
     }
 
     @Test
     fun tokenize10() {
-        assertThat(BetterTokenizer.tokenize("her--shavings,"))
+        assertThat(underTest.tokenize("her--shavings,"))
             .containsExactlyInAnyOrder("her", "shavings")
     }
 
     @Test
     fun tokenize11() {
-        assertThat(BetterTokenizer.tokenize("‘’Fingers.’’"))
+        assertThat(underTest.tokenize("‘’Fingers.’’"))
             .containsExactlyInAnyOrder("Fingers")
     }
 
     @Test
     fun tokenize12() {
-        assertThat(BetterTokenizer.tokenize("Dip-into-everything,"))
+        assertThat(underTest.tokenize("Dip-into-everything,"))
             .containsExactlyInAnyOrder("Dip", "into", "everything")
     }
 
     @Test
     fun tokenize13() {
-        assertThat(BetterTokenizer.tokenize("can’t--it"))
+        assertThat(underTest.tokenize("can’t--it"))
             .containsExactlyInAnyOrder("can", "it")
     }
 
     @Test
     fun tokenize14() {
-        assertThat(BetterTokenizer.tokenize("egg-shell."))
+        assertThat(underTest.tokenize("egg-shell."))
             .containsExactlyInAnyOrder("egg", "shell")
     }
 
     @Test
     fun tokenize15() {
-        assertThat(BetterTokenizer.tokenize("lie."))
+        assertThat(underTest.tokenize("lie."))
             .containsExactlyInAnyOrder("lie")
     }
 }
