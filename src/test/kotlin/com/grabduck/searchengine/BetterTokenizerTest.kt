6dfb160f6ca34af4e7a542e320beed96817cad3c
esurovtsev <eugene.sourovtsev@gmail.com>
2019-09-16 13:55:51 +0200

    improved unit test fro BetterTokenizer

--- src/test/kotlin/com/grabduck/searchengine/BetterTokenizerTest.kt
+++ src/test/kotlin/com/grabduck/searchengine/BetterTokenizerTest.kt
@@ -14,92 +14,26 @@ class BetterTokenizerTest {
     }
 
     @Test
-    fun tokenize01() {
-        assertThat(underTest.tokenize("‘Take"))
-            .containsExactlyInAnyOrder("Take")
-    }
+    fun tokenize() {
+        listOf(
+                "‘Take" to arrayOf("Take"),
+                "tight!’" to arrayOf("tight"),
+                "‘Don’t" to arrayOf("Don"),
+                "right!’" to arrayOf("right"),
+                "Darning-needle." to arrayOf("Darning", "needle"),
+                "Fingers;" to arrayOf("Fingers"),
+                "breast-pin!’" to arrayOf("breast", "pin"),
+                "sealing-wax.’" to arrayOf("sealing", "wax"),
+                "sha’n’t" to arrayOf("sha"),
+                "her--shavings," to arrayOf("her", "shavings"),
+                "‘’Fingers.’’" to arrayOf("Fingers"),
+                "Dip-into-everything," to arrayOf("Dip", "into", "everything"),
+                "can’t--it" to arrayOf("can", "it"),
+                "egg-shell." to arrayOf("egg", "shell"),
+                "lie." to arrayOf("lie")
 
-    @Test
-    fun tokenize02() {
-        assertThat(underTest.tokenize("tight!’"))
-            .containsExactlyInAnyOrder("tight")
-    }
-
-    @Test
-    fun tokenize03() {
-        assertThat(underTest.tokenize("‘Don’t"))
-            .containsExactlyInAnyOrder("Don")
-    }
-
-    @Test
-    fun tokenize04() {
-        assertThat(underTest.tokenize("right!’"))
-            .containsExactlyInAnyOrder("right")
-    }
-
-    @Test
-    fun tokenize05() {
-        assertThat(underTest.tokenize("Darning-needle."))
-            .containsExactlyInAnyOrder("Darning", "needle")
-    }
-
-    @Test
-    fun tokenize06() {
-        assertThat(underTest.tokenize("Fingers;"))
-            .containsExactlyInAnyOrder("Fingers")
-    }
-
-    @Test
-    fun tokenize07() {
-        assertThat(underTest.tokenize("breast-pin!’"))
-            .containsExactlyInAnyOrder("breast", "pin")
-    }
-
-    @Test
-    fun tokenize08() {
-        assertThat(underTest.tokenize("sealing-wax.’"))
-            .containsExactlyInAnyOrder("sealing", "wax")
-    }
-
-    @Test
-    fun tokenize09() {
-        assertThat(underTest.tokenize("sha’n’t"))
-            .containsExactlyInAnyOrder("sha")
-    }
-
-    @Test
-    fun tokenize10() {
-        assertThat(underTest.tokenize("her--shavings,"))
-            .containsExactlyInAnyOrder("her", "shavings")
-    }
-
-    @Test
-    fun tokenize11() {
-        assertThat(underTest.tokenize("‘’Fingers.’’"))
-            .containsExactlyInAnyOrder("Fingers")
-    }
-
-    @Test
-    fun tokenize12() {
-        assertThat(underTest.tokenize("Dip-into-everything,"))
-            .containsExactlyInAnyOrder("Dip", "into", "everything")
-    }
-
-    @Test
-    fun tokenize13() {
-        assertThat(underTest.tokenize("can’t--it"))
-            .containsExactlyInAnyOrder("can", "it")
-    }
-
-    @Test
-    fun tokenize14() {
-        assertThat(underTest.tokenize("egg-shell."))
-            .containsExactlyInAnyOrder("egg", "shell")
-    }
-
-    @Test
-    fun tokenize15() {
-        assertThat(underTest.tokenize("lie."))
-            .containsExactlyInAnyOrder("lie")
+        ).forEach {
+            assertThat(underTest.tokenize(it.first)).containsExactlyInAnyOrder(*it.second)
+        }
     }
 }
