508e78841f0d234767ef0cfb06bae5691e7c4b39
esurovtsev <eugene.sourovtsev@gmail.com>
2019-09-24 11:32:45 +0200

    simplifying code - get rid of DocumentService interface

--- src/main/kotlin/com/grabduck/searchengine/DocumentService.kt
+++ src/main/kotlin/com/grabduck/searchengine/DocumentService.kt
@@ -3,18 +3,12 @@ package com.grabduck.searchengine
 import org.springframework.stereotype.Service
 import java.io.File
 
-interface DocumentService {
-    fun findAllIds(): List<String>
-
-    fun findById(documentId: String): String?
-}
-
 @Service
-class DocumentServiceImpl(private val config: Configuration) : DocumentService {
-    override fun findAllIds(): List<String> =
+class DocumentService(private val config: Configuration) {
+    fun findAllIds(): List<String> =
             File(config.getDataDir()).listFiles().map { it.name }
 
-    override fun findById(documentId: String): String? =
+    fun findById(documentId: String): String? =
             this
                 .findAllIds()
                 .findLast { it == documentId }
