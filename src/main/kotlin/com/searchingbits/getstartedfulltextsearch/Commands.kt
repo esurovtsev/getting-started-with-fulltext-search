7af2245468adce96ee6ced844c9fbe491dbddafb
esurovtsev <eugene.sourovtsev@gmail.com>
2020-10-06 20:13:36 +0200

    part-1

--- src/main/kotlin/com/searchingbits/getstartedfulltextsearch/Commands.kt
+++ src/main/kotlin/com/searchingbits/getstartedfulltextsearch/Commands.kt
@@ -4,21 +4,10 @@ import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
 import org.springframework.shell.standard.ShellOption
 
-enum class SearchType {
-    BRUTE_FORCE, BRUTE_FORCE_TOKEN, DIRECT_INDEX, BETTER_DIRECT_INDEX, INVERTED_INDEX, GREEDY_INVERTED_INDEX, SCORING
-}
-
-enum class IndexType {
-    DIRECT, BETTER_DIRECT, INVERTED, INVERTED_DUPLICATED
-}
-
 @ShellComponent
 class Commands(
-        private val config: Configuration,
-        private val documentService: DocumentService,
-        private val searchService: SearchService,
-        private val directIndexer: DirectIndexer,
-        private val invertedIndexer: InvertedIndexer
+    private val config: Configuration,
+    private val documentService: DocumentService
 ) {
     @ShellMethod("Show current configuration")
     fun config(): List<String> =
@@ -34,38 +23,4 @@ class Commands(
             } else {
                 listOf(documentService.findById(id) ?: "document not found")
             }
-
-    @ShellMethod("Performs a search request on the documents")
-    fun search(
-            @ShellOption(help = "Defines a search type") type: SearchType,
-            @ShellOption(help = "Defines a search query") query: String
-    ): List<String> =
-            when (type) {
-                SearchType.BRUTE_FORCE -> addHeader(searchService.findUsingBruteForce_simple(query))
-                SearchType.BRUTE_FORCE_TOKEN -> addHeader(searchService.findUsingBruteForce_tokenize(query))
-                SearchType.DIRECT_INDEX -> addHeader(searchService.findUsingDirectIndex(query))
-                SearchType.BETTER_DIRECT_INDEX -> addHeader(searchService.findUsingBetterDirectIndex(query))
-                SearchType.INVERTED_INDEX -> addHeader(searchService.findUsingInvertedIndex(query))
-                SearchType.GREEDY_INVERTED_INDEX -> addHeader(searchService.findUsingGreedyInvertedIndex(query))
-                SearchType.SCORING -> addHeader(searchService.findWithScoring(query))
-            }
-
-    @ShellMethod("Generates a search Index")
-    fun createIndex(
-        @ShellOption(help = "Defines an index type") type: IndexType
-    ): String {
-        when (type) {
-            IndexType.DIRECT -> directIndexer.createIndex()
-            IndexType.BETTER_DIRECT -> directIndexer.createBetterIndex()
-            IndexType.INVERTED -> invertedIndexer.createIndex()
-            IndexType.INVERTED_DUPLICATED -> invertedIndexer.createIndexWithDuplicates()
-        }
-        return "Index created: $type"
-    }
-
-    private fun addHeader(result: Collection<String>): List<String> =
-            listOf("Results found: ${result.size}")
-                .plus("---------------------------------------")
-                .plus(result)
-                .plus("---------------------------------------")
 }
--- src/main/kotlin/com/searchingbits/getstartedfulltextsearch/Configuration.kt
+++ src/main/kotlin/com/searchingbits/getstartedfulltextsearch/Configuration.kt
@@ -3,56 +3,18 @@ package com.searchingbits.getstartedfulltextsearch
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.io.File
-import javax.annotation.PostConstruct
 
 const val DATA_DIR_KEY = "com.searching-bits.get-started-fulltext-search.data-dir"
-const val DIRECT_INDEX_DIR_KEY = "com.searching-bits.get-started-fulltext-search.direct-index-dir"
-const val STOP_WORDS_FILE_KEY = "com.searching-bits.get-started-fulltext-search.stop-words"
-const val INVERTED_INDEX_FILE_KEY = "com.searching-bits.get-started-fulltext-search.inverted-index-file"
 
 @Component
 class Configuration {
     @Value("\${$DATA_DIR_KEY}")
     private lateinit var dataDir: String
 
-    @Value("\${$DIRECT_INDEX_DIR_KEY}")
-    private lateinit var directIndexDir: String
-
-    @Value("\${$STOP_WORDS_FILE_KEY}")
-    private lateinit var stopwordsFile: String
-
-    @Value("\${$INVERTED_INDEX_FILE_KEY}")
-    private lateinit var invertedIndexFile: String
-
-    @PostConstruct
-    fun init() {
-        listOf(getDirectIndexDir())
-            .forEach {
-                val dir = File(it)
-                if (!dir.exists() || !dir.isDirectory) {
-                    dir.mkdirs()
-                }
-            }
-    }
-
     fun getAllProperties(): Map<String, String> =
-            mapOf(
-                    DATA_DIR_KEY to File(dataDir).absolutePath,
-                    DIRECT_INDEX_DIR_KEY to File(directIndexDir).absolutePath,
-                    STOP_WORDS_FILE_KEY to File(stopwordsFile).absolutePath,
-                    INVERTED_INDEX_FILE_KEY to File(invertedIndexFile).absolutePath
-            )
+            mapOf(DATA_DIR_KEY to File(dataDir).absolutePath)
 
     fun getDataDir(): String =
             getAllProperties()[DATA_DIR_KEY] ?: ".."
-
-    fun getDirectIndexDir(): String =
-            getAllProperties()[DIRECT_INDEX_DIR_KEY] ?: ".."
-
-    fun getStopWordsFile(): String =
-            getAllProperties()[STOP_WORDS_FILE_KEY] ?: "stopwords.txt"
-
-    fun getInvertedIndexFile(): String =
-            getAllProperties()[INVERTED_INDEX_FILE_KEY] ?: "index.txt"
 
 }
